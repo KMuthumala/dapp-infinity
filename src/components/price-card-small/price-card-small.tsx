fafcb304e0eb7ecb1aabcf60b4884ab174dc4f53
Krishan Muthumala <45062655+KMuthumala@users.noreply.github.com>
2022-10-24 14:41:44 +0530

    Add price line functionality with binance web socket

--- src/components/price-card-small/price-card-small.tsx
+++ src/components/price-card-small/price-card-small.tsx
@@ -2,21 +2,26 @@ import "./price-card-small.css"
 import {Card, Row} from "react-bootstrap";
 import {SmallPriceCardDetails} from "../../definitions/single-box-details";
 import Col from "react-bootstrap/Col";
+import React, {useState} from 'react';
 
-function PriceCardSmall({title, image, description,price}: SmallPriceCardDetails) {
+function PriceCardSmall({title, image, description, lastPrice, openPrice}: SmallPriceCardDetails) {
+    const [priceColour, setPriceColour] = useState("black");
 
     return (
         <div>
             <Card className={"price-card-small"}>
                 <Card.Body>
                     <Row>
-                        <Col xs={4} className={"crypto-icon"}>
-                            <img src={require(`../../images/crypto-icons/${image}.png`)} width={"40"} height={"40"}
+                        <Col xs={5} className={"crypto-icon"}>
+                            <img src={require(`../../images/crypto-icons/${image}.png`)} width={"50"} height={"50"}
                                  alt={""}/>
                         </Col>
-                        <Col xs={8}>
-                            <Card.Title>{title}</Card.Title>
-                            <Card.Text>{price}</Card.Text>
+                        <Col xs={7}>
+                            <Card.Title style={{color: "white"}}>{title}</Card.Title>
+                            <Card.Text style={{
+                                color: lastPrice > openPrice ? "green" : "red",
+                                fontWeight: "800"
+                            }}>${lastPrice}</Card.Text>
                         </Col>
                     </Row>
                 </Card.Body>
--- src/definitions/single-box-details.ts
+++ src/definitions/single-box-details.ts
@@ -1,20 +1,22 @@
 import React from "react";
 
 export interface SingleBoxDetails {
-    title:string;
-    image:string;
-    link:string;
+    title: string;
+    image: string;
+    link: string;
 }
 
 export interface SingleCardDetails {
-    title:string;
-    image:React.ReactNode;
-    description:string;
+    title: string;
+    image: React.ReactNode;
+    description: string;
 }
 
 export interface SmallPriceCardDetails {
-    title:string;
-    description:string;
-    image:string;
-    price:number;
+    title: string;
+    description: string;
+    image: string;
+    lastPrice: number,
+    openPrice: number
+
 }
