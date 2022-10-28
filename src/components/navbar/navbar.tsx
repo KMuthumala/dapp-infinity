2f104f311f6764dbb331b36c51626d3f8b23084c
Krishan Muthumala <45062655+KMuthumala@users.noreply.github.com>
2022-10-28 21:58:18 +0530

    Implement Navbar tags to be highlighted

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,14 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import "./navbar.css"
-import {Button} from "react-bootstrap";
+
 
 function NavBarComponent() {
     const navbarTags=[
-        {tag:"Defi",to:"dapps"},
-        {tag:"NFTs",to:""},
-        {tag:"AirDrops",to:""},
-        {tag:"Blog",to:""},
+        {tag:"Dapps",to:"dapps"},
+        {tag:"NFTs",to:"nft"},
+        {tag:"AirDrops",to:"airdrops"},
+        {tag:"Blog",to:"blogs"},
     ];
 
     return (
@@ -21,7 +21,7 @@ function NavBarComponent() {
                     <Nav className="me-auto">
                     </Nav>
                     <Nav className="ms-5 tags_styles" >
-                        {navbarTags.map((e)=>(<Nav.Link key={e.tag} className="me-4" href={e.to}>{e.tag}</Nav.Link> ))}
+                        {navbarTags.map((e)=>(<Nav.Link key={e.tag} active={window.location.pathname==="/"+e.to} className="me-4" href={e.to}>{e.tag}</Nav.Link> ))}
                     </Nav>
                     {/*<Button className="button-secondary">Connect Wallet</Button>*/}
                 </Navbar.Collapse>
